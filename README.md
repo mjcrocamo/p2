9ca0f292cdde80b7b9ba76bdb48f2e3fe385d054
Maggie Crocamo <mjcrocamo@gmail.com>
2018-09-22 19:16:47 -0400

    adding files for first commit and testing

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# p2
-Project 2 for DWA15
+# Project 2
++ By: Maggie Crocamo
++ Production URL: <http://p2.maggiecroc11.me>
+
+## Outside resources
+*your list of outside resources go here*
+
+## 3 Unique inputs
+
+## Class
+
+## Code style divergences
+
+## Notes for instructor
+
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require 'logic.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+          rel='stylesheet'
+          integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO'
+          crossorigin='anonymous'>
+    <link rel="stylesheet" type="text/css" media="screen" href="/styles/styles.css" />
+    <title>Dad's the Word</title>
+</head>
+<body>
+
+    <h1>Dad's the Word</h1>   
+
+</body>
+</html>
